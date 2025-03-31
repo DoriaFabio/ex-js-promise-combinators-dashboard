8d4299f4b560bd0651f5a69401ab3e977b4018ef
DoriaFabio <doriafabio2001@gmail.com>
2025-03-31 17:56:13 +0200

    do dashboard city

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # ex-js-promise-combinators-dashboard
+https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}
+
+# Chiamate a catena
+const eseguiTask = (durata) => {
+    return new Promise((res, rej) => {
+        if (!durata) {
+            rej("Non hai fornito una durata");
+        } else {
+            setTimeout(() => {
+                res(`Task effettuata dopo ${durata}ms`);
+            }, durata);
+        }
+    });
+}
+
+(async () => {
+    const promise1 = eseguiTask(700);
+    const promise2 = eseguiTask(1000);
+    const promise3 = eseguiTask(500);
+    const messaggi = await Promise.all([promise1, promise2, promise3]);
+    console.log(messaggi);
+})();
+
+Se non inserisco una durata a una promise, vanno in errore tutte le promise. Posso mettere in un try/catch il codice per avere il messaggio di errore.
+
+# getUser a catena
+const getUser = async id => {
+    const res = await fetch(`https://dummyjson.com/users/${id}`);
+    const user = await res.json();
+    return user;
+}
+
+(async () => {
+    const promises = [];
+    for (let id = 1; id <= 5; id++) {
+        const userPromise = await getUser(id);
+        promises.push(userPromise);
+    }
+    const users = await Promise.all(promises);
+    console.log(users);
+})();
---
+++ script.js
@@ -0,0 +1,44 @@
+async function fetchJson(url) { 
+    const res = await fetch(url);
+    const obj = await res.json();
+    return obj;
+} 
+
+const getDashboardData = async query => {
+    try {
+        const desPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`);
+        const wetPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`);
+        const airPromise = fetchJson(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`);
+        const promises = [desPromise, wetPromise, airPromise];
+        const [destination, weather, airport] = await Promise.all(promises); //? Destrutturazione
+        // const travel = await Promise.all(promises); //? Senza destrutturazione
+        console.log();
+        return {
+            // city: travel[0][0].name,
+            // country: travel[0][0].country,
+            // temperature: travel[1][0].temperature,
+            // weather: travel[1][0].weather_description,
+            // airport: travel[2][0].name
+            city: destination[0].name,
+            country: destination[0].country,
+            temperature: weather[0].temperature,
+            weather: weather[0].weather_description,
+            airport: airport[0].name
+        }
+    } catch(err) {
+        throw new Error(`Errore nel recupero dei dati ${err.message}`);
+    }
+    
+}
+
+getDashboardData("london")
+    .then(data => {
+        console.log('Dasboard data:', data);
+        console.log(
+            `${data.city} is in ${data.country}.\n` +
+            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
+            `The main airport is ${data.airport}.\n`
+        );
+    })
+    .catch(error => console.error(error))
+    .finally(console.log("Fine"));
